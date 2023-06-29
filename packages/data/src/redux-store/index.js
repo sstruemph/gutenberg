318a58fa0828b5982ff90c599362d7fbab45c289
Dave Smith <getdavemail@gmail.com>
2023-06-29 13:24:58 +0100

    Add optional method property to normalize arguments before usage

--- packages/data/src/redux-store/index.js
+++ packages/data/src/redux-store/index.js
@@ -601,6 +601,9 @@ function mapSelectorWithResolver(
 	}
 
 	const selectorResolver = ( ...args ) => {
+		if ( resolver.normalizeArgs ) {
+			args = resolver.normalizeArgs( args );
+		}
 		fulfillSelector( args );
 		return selector( ...args );
 	};
