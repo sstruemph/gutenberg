bc268ea8d9a0b79b6dbefc82abbdca1105c80e0b
Dave Smith <getdavemail@gmail.com>
2023-07-05 13:28:54 +0100

    Apply types optimisation from code review
    See https://github.com/WordPress/gutenberg/pull/52120#discussion_r1253011269

--- packages/core-data/src/selectors.ts
+++ packages/core-data/src/selectors.ts
@@ -102,6 +102,13 @@ type Optional< T > = T | undefined;
 type GetRecordsHttpQuery = Record< string, any >;
 
 /**
+ * Arguments for EntityRecord selectors.
+ */
+type EntityRecordArgs =
+	| [ string, string, EntityRecordKey ]
+	| [ string, string, EntityRecordKey, GetRecordsHttpQuery ];
+
+/**
  * Shared reference to an empty object for cases where it is important to avoid
  * returning a new object reference on every invocation, as in a connected or
  * other pure component which performs `shouldComponentUpdate` check on props.
@@ -276,17 +283,16 @@ export function getEntityConfig(
  * See https://github.com/WordPress/gutenberg/pull/41578 for more details.
  */
 export interface GetEntityRecord {
-	normalizeArgs: ( args: any ) => any;
 	<
 		EntityRecord extends
 			| ET.EntityRecord< any >
 			| Partial< ET.EntityRecord< any > >
 	>(
 		state: State,
-		kind: string,
-		name: string,
-		key: EntityRecordKey,
-		query?: GetRecordsHttpQuery
+		kind: EntityRecordArgs[ 0 ],
+		name: EntityRecordArgs[ 1 ],
+		key: EntityRecordArgs[ 2 ],
+		query?: EntityRecordArgs[ 3 ]
 	): EntityRecord | undefined;
 
 	CurriedSignature: <
@@ -294,11 +300,12 @@ export interface GetEntityRecord {
 			| ET.EntityRecord< any >
 			| Partial< ET.EntityRecord< any > >
 	>(
-		kind: string,
-		name: string,
-		key: EntityRecordKey,
-		query?: GetRecordsHttpQuery
+		kind: EntityRecordArgs[ 0 ],
+		name: EntityRecordArgs[ 1 ],
+		key: EntityRecordArgs[ 2 ],
+		query?: EntityRecordArgs[ 3 ]
 	) => EntityRecord | undefined;
+	normalizeArgs?: ( args: EntityRecordArgs ) => EntityRecordArgs;
 }
 
 /**
